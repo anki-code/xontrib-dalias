026b297fa9e45b3f6ae6d37bca3ca2f16fb4291c
Andy Kipp <anki-code@users.noreply.github.com>
2024-07-15 12:05:16 +0200

    Update __init__.py

--- xontrib/spec_mod/__init__.py
+++ xontrib/spec_mod/__init__.py
@@ -14,11 +14,39 @@ __all__ = ()
 
 _imp = type('ImpCl', (object,), {'__getattr__':lambda self, name: __import__(name)})()  # Sugar for inline import
 
+#
+# Format output
+#
+
 XSH.aliases['@lines'] = _mod({"output_format": 'list_lines'}, "Set `list_lines` output format.")
+
+def _split_output(output_lines):
+    """Split lines by whitespaces using xonsh lexer.
+    The source code is from `xonsh.built_ins.subproc_captured_inject` (it's `@$()` operator logic).
+    """
+    toks = []
+    for line in output_lines:
+        line = line.rstrip(os.linesep)
+        toks.extend(__xonsh__.execer.parser.lexer.split(line))
+    return toks
+
+XSH.aliases['@parts'] = _mod({"output_format": _split_output}, "Output format: list of parts like in `@$()`.")
+
+
+#
+# Return object
+#
+
 XSH.aliases['@json'] = _mod({"output_format": lambda lines: _imp.json.loads('\n'.join(lines))}, "Return `json` output format.")
 XSH.aliases['@dict'] = _mod({"output_format": lambda lines: load_as_dict('\n'.join(lines))['dict']}, "Return `dict` output format.")
 XSH.aliases['@path'] = _mod({"output_format": lambda lines: _imp.pathlib.Path(':'.join(lines))}, "Return `path` output format.")
 XSH.aliases['@yaml'] = _mod({"output_format": lambda lines: _imp.yaml.safe_load('\n'.join(lines))}, "Return `yaml` output format.")
 
+#
+# Error handling
+#
+
 XSH.aliases['@err'] = _mod({"raise_subproc_error": True}, "Set `raise_subproc_error` to True.")
 XSH.aliases['@noerr'] = _mod({"raise_subproc_error": False}, "Set `raise_subproc_error` to False.")
+
+
